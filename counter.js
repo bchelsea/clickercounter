a53f5c670bfc9271df12b0bb04cc2cb99c969ada
chelsea barocio <barocio.chelsea@gmail.com>
2021-03-03 14:28:09 -0800

    first commit, created html,css,js, and created function for clicker counter

---
+++ counter.js
@@ -0,0 +1,9 @@
+
+//declare the variable counter starts at 0
+var counter = 0;
+//write function that gets the counter and increments by one every "onClick"
+function clicked(myFunction) {
+    counter = counter + 1;
+    //log out the click count
+    console.log("click count: " + counter)
+}
